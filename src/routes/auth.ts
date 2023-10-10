126e7b5993be754b87792b3205d8e2520ad12588
David O'Brien <davidob323@gmail.com>
2023-10-10 16:20:37 -0400

    quick changes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -4,31 +4,34 @@ import HttpError from "../types/errors";
 import { success, successStatus } from "../util/api";
 import passport from "passport";
 import User from "../models/auth";
-import { Strategy } from "passport-local";
+import { Strategy as LocalStrategy } from "passport-local";
 
 const router = Router();
 
 console.log("hi");
 
 router.post("/signup", (req, res, next) => {
-  const { email, password } = req.body;
-  if (typeof(email) !== "string" || typeof(password) !== "string") {
-    res.status(400).send({message: "Must have fields email and password"});
-  } else {
-    //store email and password in db
-    res.send("Sign up completed")
-  }
+    const { email, password } = req.body;
+    if (typeof email !== "string" || typeof password !== "string") {
+        res.status(400).send({ message: "Must have fields email and password" });
+    } else {
+        //store email and password in db
+        res.send("Sign up completed");
+    }
 });
 
-// https://www.npmjs.com/package/passport-local-mongoose 
+// https://www.npmjs.com/package/passport-local-mongoose
 router.post("/login", (req, res, next) => {
-  passport.use(new Strategy(User.authenticate()));
-  //passport.serializeUser(User.serializeUser());
-  passport.deserializeUser(User.deserializeUser());
+    passport.use(new LocalStrategy(User.authenticate()));
+    passport.serializeUser(
+        User.serializeUser() as (user: Express.User, cb: (err: any, id?: any) => void) => void
+    );
+    passport.deserializeUser(User.deserializeUser());
+    const hi = User.serializeUser();
 });
 
 router.post("/logout", (req, res, next) => {
-    success
+    success;
 });
 
 export default router;
