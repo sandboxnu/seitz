e651f58180e3a8ce3993db278edbd21d562a62e4
Ross Newman <rzn3102@gmail.com>
2023-10-10 23:53:12 -0400

    Combine API + UI into one monorepo (#12)
    Co-authored-by: gmanalang <gabrielmanalang711@gmail.com>
    Co-authored-by: Ross Newman <ross@ross-m2.local>
    Co-authored-by: Gabriel Manalang <123319773+gmanalang@users.noreply.github.com>

---
+++ packages/ui/src/components/AppNavbar.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+</script>
+
+<template>
+  <ElMenu :default-active="route.path" mode="horizontal" :router="true">
+    <ElMenuItem index="/">Hello</ElMenuItem>
+    <ElMenuItem index="/test">Test</ElMenuItem>
+    <ElMenuItem index="/example">Example</ElMenuItem>
+  </ElMenu>
+</template>
---
+++ packages/ui/src/main.ts
@@ -0,0 +1,13 @@
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import router from './router';
+import ElementPlus from 'element-plus';
+import { VueQueryPlugin } from '@tanstack/vue-query';
+import 'element-plus/dist/index.css';
+
+createApp(App)
+  .use(router)
+  .use(ElementPlus)
+  .use(VueQueryPlugin)
+  .mount('#app');
