d53eec657d83c9383d0673a6025ed3c74dce801e
David O'Brien <davidob323@gmail.com>
2023-10-13 12:05:17 -0400

    new changes

--- src/models/auth.ts
+++ src/models/auth.ts
@@ -1,17 +1,23 @@
 // User
 
-import { Schema, model } from 'mongoose';
+import { Schema, model } from "mongoose";
 
-export interface IUser {
-  email: string;
-  password: string;
+export interface IUser extends Document {
+    email: string;
+    password: string;
+    verifyPassword: (password: string) => boolean;
 }
 
 const userSchema = new Schema<IUser>({
-  email: { type: String, required: true },
-  password: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
 });
 
-const User = model<IUser>('User', userSchema);
+// TODO: Implement encryption
+userSchema.methods.verifyPassword = function (password: string) {
+    return this.password === password;
+};
+
+const User = model<IUser>("User", userSchema);
 
 export default User;
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,12 +3,26 @@ import HttpError from "../types/errors";
 
 import { success, successStatus } from "../util/api";
 import passport from "passport";
-import User from "../models/auth";
+import User, { IUser } from "../models/auth";
 import { Strategy as LocalStrategy } from "passport-local";
 
 const router = Router();
 
-//passport.use(new LocalStrategy)
+passport.use(
+    new LocalStrategy((email, password, done) => {
+        User.findOne({ email }, (err: Error, user: IUser | false) => {
+            if (err) {
+                return done(err);
+            } else if (!user) {
+                return done(null, false, { message: "No user for the given email" });
+            } else if (!user.verifyPassword(password)) {
+                return done(null, false, { message: "The given password is incorrect" });
+            } else {
+                return done(null, user);
+            }
+        });
+    })
+);
 
 // https://stackoverflow.com/questions/32398120/passport-allow-sign-up-with-name-and-email-address-local-strategy
 router.post("/signup", (req, res, next) => {
@@ -18,26 +32,16 @@ router.post("/signup", (req, res, next) => {
     } else {
         //store email and password in db
         const newUser = new User({ email, password });
-        //newUser.save
-        
-        User.register(newUser, password, (err) => {
-            if (err) {
-                res.status(400).send(err.message)
-            } else {
-                res.send("Sign up completed");
-            }
-            
-          
-
-        });
+        newUser
+            .save()
+            .then(({ email }) => res.send(`Sign up for ${email} has been successful.`))
+            .catch((error: Error) => res.status(400).send(error.message));
     }
 });
 
 // https://www.npmjs.com/package/passport-local-mongoose
 router.post("/login", passport.authenticate("local"), (req, res, next) => {
-    
     res.send("Login successful");
-
 });
 
 router.post("/logout", (req, res, next) => {
