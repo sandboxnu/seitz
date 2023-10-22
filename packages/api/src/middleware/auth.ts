2709ee7ae1150f8699b71525d1e6ddfe47c3c1c5
Amber <amberhorace@gmail.com>
2023-10-22 15:56:27 -0400

    created authentication check middleware and added to /auth/logout route

---
+++ packages/api/src/middleware/auth.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from 'express';
+
+const isAuthenticated = function (req: Request, res: Response, next: NextFunction) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.status(401).send("Unauthenticated user").redirect('/login');
+}
+
+export default isAuthenticated;
