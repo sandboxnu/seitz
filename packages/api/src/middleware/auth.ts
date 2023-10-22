9818f1b888b347638c83928ca0f22b23c9e4a6cb
Amber <amberhorace@gmail.com>
2023-10-22 16:05:24 -0400

    prettier check

--- packages/api/src/middleware/auth.ts
+++ packages/api/src/middleware/auth.ts
@@ -1,10 +1,14 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from "express";
 
-const isAuthenticated = function (req: Request, res: Response, next: NextFunction) {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    res.status(401).send("Unauthenticated user").redirect('/login');
-}
+const isAuthenticated = function (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.status(401).send("Unauthenticated user").redirect("/login");
+};
 
 export default isAuthenticated;
