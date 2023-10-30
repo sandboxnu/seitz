e7637864d5fe0f8b0c2f2555f299609fc001f458
Ross Newman <rzn3102@gmail.com>
2023-10-29 20:31:05 -0400

    example drag+drop component

---
+++ packages/ui/src/pages/CarPage/components/DraggableList.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useDragStore } from "@/stores/drag";
+import { ref } from "vue";
+import Draggable from "vuedraggable";
+
+const dragStore = useDragStore();
+
+defineProps<{ idx: number }>();
+
+const drag = ref(false);
+</script>
+
+<template>
+  <Draggable
+    v-model="dragStore.lists[idx]"
+    group="session-elements"
+    @start="drag = true"
+    @end="drag = false"
+    item-key="id"
+  >
+    <template #item="{ element }">
+      <div class="cursor-pointer">{{ element }}</div>
+    </template>
+  </Draggable>
+</template>
---
+++ packages/ui/src/stores/drag.ts
@@ -0,0 +1,10 @@
+import { defineStore } from "pinia";
+
+export const useDragStore = defineStore("drag", () => {
+  const lists = [
+    ["thing 1", "thing2"],
+    ["thing 3", "thing 4"],
+  ];
+
+  return { lists };
+});
