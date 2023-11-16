90c9fc3247f4682917890607144e1502d7c39c41
gmanalang <gabrielmanalang711@gmail.com>
2023-11-16 03:32:00 -0500

    add save changes button

--- packages/ui/src/pages/DashboardPage/components/StudyPanel.vue
+++ packages/ui/src/pages/DashboardPage/components/StudyPanel.vue
@@ -29,16 +29,30 @@ const draggableProps = {
   handle: ".handle",
   animation: 200,
 };
+
+const saveChanges = () => {
+  console.log("changes saved");
+};
 </script>
 
 <template>
   <div class="flex flex-col border border-black overflow-x-hidden p-5">
-    <h1 class="text-4xl">
-      <input type="text" placeholder="Untitled Study" />
-    </h1>
-    <h2 class="text-2xl my-2">
-      <input type="text" placeholder="Add a description" />
-    </h2>
+    <div class="flex flex-row items-center justify-between">
+      <div class="flex-none w-4/5">
+        <h1 class="text-4xl">
+          <input type="text" placeholder="Untitled Study" />
+        </h1>
+        <h2 class="text-2xl my-2">
+          <input type="text" placeholder="Add a description" />
+        </h2>
+      </div>
+      <button
+        class="text-base bg-gray-200 border border-black rounded-lg px-5 py-1 h-auto justify-center"
+        @click="saveChanges"
+      >
+        Save Changes
+      </button>
+    </div>
     <div
       class="grow flex flex-row border-2 border-black rounded-xl p-2 overflow-x-auto"
     >
