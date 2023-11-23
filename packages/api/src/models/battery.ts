0715ced54cf41891737f4bb6fad7d3c16f2d6f9f
Ross Newman <rzn3102@gmail.com>
2023-11-23 02:32:21 -0500

    battery hierarchy models

--- packages/api/src/models/battery.ts
+++ packages/api/src/models/battery.ts
@@ -1,4 +1,4 @@
-import { Types } from "mongoose";
+import { Schema, Types, model } from "mongoose";
 
 export interface IGenericOption<T> {
   name: string;
@@ -7,14 +7,14 @@ export interface IGenericOption<T> {
 
 export interface INumberOption extends IGenericOption<number> {
   type: "number";
-  min: number;
-  max: number;
-  step: number;
+  min?: number;
+  max?: number;
+  step?: number;
 }
 
 export interface ITextOption extends IGenericOption<string> {
   type: "text";
-  maxLength: number;
+  maxLength?: number;
 }
 
 export interface IDropdownOption extends IGenericOption<number> {
@@ -45,24 +45,66 @@ export interface IBattery {
   stages: Types.ObjectId[];
 }
 
-export interface IOptionValue<T> {
-  option: IGenericOption<T> & IOption;
-  value: T;
-}
+const optionSchema = new Schema<IOption>(
+  {
+    name: { type: String, required: true },
+  },
+  { discriminatorKey: "type" }
+);
 
-export interface ICompleteBattery {
-  battery: Types.ObjectId;
-  values: { optionId: Types.ObjectId; value: unknown }[];
-}
+const batteryStageSchema = new Schema<IBatteryStage>({
+  type: { type: String, required: true },
+  stageLabel: { type: String, required: true },
+  options: [optionSchema],
+});
 
-// const optionSchema = new Schema<IOption>({
-//   name: String,
+const optArray = batteryStageSchema.path<Schema.Types.DocumentArray>("options");
 
-// });
+const numberOptionSchema = new Schema<INumberOption>({
+  default: { type: Number, required: true },
+  min: Number,
+  max: Number,
+  step: Number,
+});
 
-// const batterySchema = new Schema<IBattery>({
-//   name: String,
-//   description: String,
-//   imageUrl: String,
-//   options: []
-// })
+const textOptionSchema = new Schema<ITextOption>({
+  default: { type: String, required: true },
+  maxLength: Number,
+});
+
+const dropdownOptionSchema = new Schema<IDropdownOption>({
+  default: { type: Number, required: true },
+  options: [String],
+});
+
+const checkboxOptionSchema = new Schema<ICheckboxOption>({
+  default: { type: Boolean, required: true },
+});
+
+export const NumberOption = optArray.discriminator(
+  "number",
+  numberOptionSchema
+);
+export const TextOption = optArray.discriminator("text", textOptionSchema);
+export const DropdownOption = optArray.discriminator(
+  "dropdown",
+  dropdownOptionSchema
+);
+export const CheckboxOption = optArray.discriminator(
+  "checkbox",
+  checkboxOptionSchema
+);
+
+export const BatteryStage = model<IBatteryStage>(
+  "BatteryStage",
+  batteryStageSchema
+);
+
+const batterySchema = new Schema<IBattery>({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  stages: [{ type: Schema.Types.ObjectId, ref: "BatteryStage" }],
+});
+
+export const Battery = model<IBattery>("Battery", batterySchema);
