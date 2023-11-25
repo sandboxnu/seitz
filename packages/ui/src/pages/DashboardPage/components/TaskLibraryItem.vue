63d12d62e603427f5598abff44401e2b83e206e4
Ross Newman <rzn3102@gmail.com>
2023-11-25 01:03:12 -0500

    library tooltips

--- packages/ui/src/pages/DashboardPage/components/TaskLibraryItem.vue
+++ packages/ui/src/pages/DashboardPage/components/TaskLibraryItem.vue
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 defineProps<{
   selected: boolean;
   name: string;
+  description: string;
 }>();
 defineEmits(["flip"]);
 
@@ -11,29 +12,35 @@ const uuid = crypto.randomUUID();
 </script>
 
 <template>
-  <div
-    :class="[
-      'relative w-40 h-40 rounded-3xl border-black overflow-hidden',
-      selected ? 'border-4' : 'border cursor-pointer',
-    ]"
-    @click="if (!selected) $emit('flip');"
-  >
-    <ElImage
-      :src="`https://picsum.photos/300/300?${uuid}`"
-      fit="cover"
-      class="h-full w-full"
-    />
-    <div
-      v-if="selected"
-      class="absolute w-full h-full top-0 left-0 bg-white/60"
-    ></div>
-    <div class="absolute right-3 bottom-3">
+  <div class="w-40">
+    <ElTooltip effect="dark" :content="description" placement="right">
       <div
-        class="rounded-full h-6 w-6 bg-black text-white flex justify-center items-center"
+        :class="[
+          'relative w-full h-40 rounded-3xl border-black overflow-hidden',
+          selected ? 'border-4' : 'border cursor-pointer',
+        ]"
+        @click="if (!selected) $emit('flip');"
       >
-        <FontAwesomeIcon :icon="['fas', selected ? 'check' : 'plus']" />
+        <ElImage
+          :src="`https://picsum.photos/300/300?${uuid}`"
+          fit="cover"
+          class="h-full w-full"
+        />
+        <div
+          v-if="selected"
+          class="absolute w-full h-full top-0 left-0 bg-white/60"
+        ></div>
+        <div class="absolute right-3 bottom-3">
+          <div
+            class="rounded-full h-6 w-6 bg-black text-white flex justify-center items-center"
+          >
+            <FontAwesomeIcon :icon="['fas', selected ? 'check' : 'plus']" />
+          </div>
+        </div>
       </div>
-    </div>
+    </ElTooltip>
+    <ElTooltip effect="dark" :content="name" placement="bottom">
+      <div class="text-base text-black truncate">{{ name }}</div>
+    </ElTooltip>
   </div>
-  <div class="text-base text-black">{{ name }}</div>
 </template>
