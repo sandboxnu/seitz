7c9858ec460d97f9327558bf2560c47672fc6454
Amber Chow <118627484+amberchow8@users.noreply.github.com>
2024-03-24 15:43:10 -0400

    Added Tailwind theme (#107)

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -3,7 +3,33 @@ export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   important: true,
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#FFEDE9",
+          100: "#FF8773",
+          200: "#EB4D38",
+          300: "#C82610",
+          400: "#8C1B0B",
+          500: "#5C0D02",
+        },
+        accent: {
+          100: "#A4804A",
+          300: "#7B6038",
+          500: "#524025",
+        },
+        neutral: {
+          10: "#FFFDFD",
+          50: "#FAF7F6",
+          100: "#F1EDEA",
+          200: "#E6DFD8",
+          300: "#C3BCB5",
+          400: "#8A7F75",
+          500: "#594F47",
+          600: "#1F1915",
+        },
+      },
+    },
   },
   plugins: [],
 };
