6135634f1c531c8cf73fe37ddb2795bc3d3f9521
Amber <amberhorace@gmail.com>
2024-03-26 21:46:15 -0400

    implemented basic email verification using sendgrid

--- packages/api/src/routes/auth.ts
+++ packages/api/src/routes/auth.ts
@@ -4,6 +4,8 @@ import { User } from "../models";
 import { Strategy as LocalStrategy } from "passport-local";
 import isAuthenticated from "../middleware/auth";
 import HttpError from "../types/errors";
+import sgMail from "@sendgrid/mail";
+import crypto from "crypto";
 
 const router = Router();
 
@@ -42,8 +44,26 @@ router.post("/signup", (req, res, next) => {
   if (typeof email !== "string" || typeof password !== "string") {
     next(new HttpError(400, "Must have fields email and password"));
   } else {
-    User.create({ email, password })
+    const token = crypto.randomBytes(20).toString("hex");
+    User.create({ email, password, token })
       .then(async (user) => {
+        sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
+        const URL = `http://localhost:4000/auth/verify/${user.token}`;
+        const msg = {
+          to: `${email}`,
+          from: "chow.am@northeastern.edu", // Change to your verified sender
+          subject: "Brain Game Center: Verify Your Email",
+          html: `<p>Welcome to Brain Game Center! Please verify your email by clicking this <a href="${URL}">link</a>.</p>`,
+        };
+        sgMail
+          .send(msg)
+          .then(() => {
+            console.log("Email sent");
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+
         req.login(user, (err) => {
           if (!err) res.sendStatus(201);
           else next(err);
@@ -79,6 +99,22 @@ router.post("/logout", isAuthenticated, (req, res, next) => {
 
 router.get("/user", isAuthenticated, (req, res) => {
   res.json(req.user);
+});
+
+router.get("/verify/:token", async (req, res) => {
+  const token = req.params.token;
+  try {
+    const user = await User.findOne({ token: token });
+    if (!user) {
+      return res.status(404).send("Token not found");
+    }
+    user.verified = true;
+    user.token = "";
+    await user.save();
+    res.status(200).send("Verified!");
+  } catch (err) {
+    res.status(500).send("Could not verify: " + err);
+  }
 });
 
 export default router;
