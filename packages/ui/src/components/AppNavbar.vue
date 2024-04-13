6d531943b86998d6846f1b777fcef31d125497f4
gmanalang <gabrielmanalang711@gmail.com>
2024-04-13 18:31:14 -0400

    fixed navbar

--- packages/ui/src/components/AppNavbar.vue
+++ packages/ui/src/components/AppNavbar.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import { useAuthStore } from "@/stores/auth";
 
 const route = useRoute();
 const router = useRouter();
 const authStore = useAuthStore();
+const isCollapsed = ref(false);
 
 function logOut() {
   authStore.logOut();
@@ -14,30 +16,64 @@ function logOut() {
 
 <template>
   <ElMenu
-    class="border-b-0 flex items-center"
+    :class="[
+      'border-b-0 flex flex-col h-full shrink-0',
+      isCollapsed ? 'w-16' : 'w-44',
+    ]"
     :default-active="route.path"
-    mode="horizontal"
+    mode="vertical"
     :router="true"
     :ellipsis="false"
     background-color="black"
     text-color="white"
     active-text-color="white"
   >
-    <RouterLink
-      to="/"
-      class="text-white text-xl pl-6 mr-2 pb-1 flex items-center"
-    >
-      <span class="font-light">Northeastern</span>&nbsp;
-      <span class="font-bold">Brain Game Center</span>
-    </RouterLink>
+    <ElImage
+      v-if="!isCollapsed"
+      src="/icons/ep_d-arrow-left.svg"
+      class="h-5 w-5 mt-[22px] mr-3.5 self-end cursor-pointer"
+      @click="isCollapsed = true"
+    />
+    <ElImage
+      v-else
+      src="/icons/ep_d-arrow-right.svg"
+      class="h-5 w-5 mt-[22px] self-center cursor-pointer"
+      @click="isCollapsed = false"
+    />
+    <ElMenuItem class="flex flex-row items-center gap-3">
+      <ElImage src="/icons/home.svg" />
+      <template v-if="!isCollapsed">Home</template>
+    </ElMenuItem>
     <template v-if="authStore.currentUser">
-      <ElMenuItem index="/studies">My Studies</ElMenuItem>
-      <div class="flex-1"></div>
+      <ElMenuItem index="/studies" class="flex flex-row items-center gap-3">
+        <ElImage src="/icons/ep_notebook.svg" />
+        <template v-if="!isCollapsed">My Studies</template>
+      </ElMenuItem>
+      <ElMenuItem class="flex flex-row items-center gap-3">
+        <ElImage src="/icons/ep_user-filled.svg" />
+        <template v-if="!isCollapsed">Profile</template>
+      </ElMenuItem>
       <template v-if="authStore.currentUser.isAdmin">
-        <ElMenuItem index="/admin">Admin</ElMenuItem>
+        <ElImage src="/icons/Line 29.svg" class="mx-6 my-9" />
+        <ElMenuItem index="/admin" class="flex flex-row items-center gap-3">
+          <ElImage src="/icons/Vector.svg" />
+          <template v-if="!isCollapsed">Task Templates</template>
+        </ElMenuItem>
+        <ElMenuItem class="flex flex-row items-center gap-3">
+          <ElImage src="/icons/bi_people-fill.svg" />
+          <template v-if="!isCollapsed">Users</template>
+        </ElMenuItem>
       </template>
-      <ElMenuItem index="/logout" :onclick="logOut">
-        Log Out, {{ authStore.currentUser.email }}
+      <div class="grow"></div>
+      <ElMenuItem
+        index="/logout"
+        :onclick="logOut"
+        class="flex flex-row items-center gap-3"
+      >
+        <ElImage src="/icons/ep_back.svg" />
+        <template v-if="!isCollapsed">
+          Log Out, {{ authStore.currentUser.email }}
+        </template>
       </ElMenuItem>
     </template>
     <template v-else>
@@ -45,6 +81,12 @@ function logOut() {
       <ElMenuItem index="/login">Log In</ElMenuItem>
       <ElMenuItem index="/signup">Sign Up</ElMenuItem>
     </template>
+    <div class="h-20 text-lg text-white pl-3.5 mt-4">
+      <RouterLink v-if="!isCollapsed" to="/" class="flex flex-col items-left">
+        <span class="font-light">Northeastern</span>
+        <span class="font-bold">Brain Game Center</span>
+      </RouterLink>
+    </div>
   </ElMenu>
 </template>
 
