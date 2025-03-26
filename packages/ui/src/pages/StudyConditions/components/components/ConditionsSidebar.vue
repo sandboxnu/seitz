ded4912744c64f88e70e38973e660a209c277657
David O'Brien <davidob323@gmail.com>
2025-03-26 19:44:41 -0400

    oop

--- packages/ui/src/pages/StudyConditions/components/components/ConditionsSidebar.vue
+++ packages/ui/src/pages/StudyConditions/components/components/ConditionsSidebar.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useStudyBuilderStore } from "@/stores/studyBuilder";
 import { ref } from "vue";
+import SessionCard from "./SessionCard.vue";
 
 const selectedOption = ref("Details");
 const studyBuilderStore = useStudyBuilderStore();
@@ -62,35 +63,21 @@ const studyBuilderStore = useStudyBuilderStore();
           </div>
         </div>
         <div v-else-if="selectedOption === 'Sessions'">
-          <div class="flex flex-col space-y-4 overflow-y-auto max-h-[400px]">
-            <p
-              v-for="(session, index) in studyBuilderStore.sessions"
-              :key="index"
-            >
-              {{ session }}
-            </p>
+          <div
+            v-drag-and-drop:group="'sessions'"
+            class="flex flex-col space-y-4 overflow-y-auto max-h-[400px]"
+          >
+            <SessionCard
+              v-for="(sessionId, index) in studyBuilderStore.sessions"
+              :key="sessionId"
+              :session-id="sessionId"
+              :index="index + 1"
+              draggable
+              class="w-[264px] shrink-0"
+            />
           </div>
         </div>
       </div>
     </div>
   </div>
 </template>
-
-<style scoped>
-.slide-leave-active,
-.slide-enter-active {
-  transition: 0.75s;
-  transition-property: width;
-  overflow: hidden;
-}
-
-.slide-enter-to,
-.slide-leave-from {
-  width: 764px;
-}
-
-.slide-enter-from,
-.slide-leave-to {
-  width: 0;
-}
-</style>
