db2dbbbe160752e0da21639340e85fe9c25d21e1
jolinhuang224 <huang.jol@northeastern.edu>
2025-09-25 18:23:28 -0400

    spacing

--- packages/ui/src/pages/MyStudiesPage/components/RecentStudies.vue
+++ packages/ui/src/pages/MyStudiesPage/components/RecentStudies.vue
@@ -16,6 +16,7 @@ const props = withDefaults(defineProps<Props>(), {
     "Perhaps some supplemental description can go here and but idk how long and what information they want",
   variants: 50,
   lastModified: "2024-10-24",
+  //don't really have different statuses to pull from yet?
   status: "Editing",
 });
 
@@ -47,10 +48,10 @@ const isoDate = computed(() =>
 <template>
   <!-- TODO: repeat and display 3 most recent studies -->
   <article
-    class="p-6 sm:p-8 my-6 sm:my-10 bg-white rounded-xl ring-1 ring-stone-300/90 shadow-sm flex flex-col gap-3 max-w-lg"
+    class="sm:p-7 sm:my-9 bg-white rounded-xl ring-1 ring-stone-300/90 shadow-sm flex flex-col gap-3 max-w-md"
     :aria-label="`${title} card`"
   >
-    <header class="flex items-start justify-between gap-3">
+    <header class="flex items-start justify-between">
       <h3 class="text-stone-900 text-2xl font-bold leading-7">{{ title }}</h3>
     </header>
 
