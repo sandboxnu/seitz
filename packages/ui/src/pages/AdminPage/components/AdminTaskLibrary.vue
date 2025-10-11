c23009cfb4a189c3fe5fe90bc0a1b3f84a04f077
jasminduong <jasmind009@gmail.com>
2025-10-11 16:15:01 -0400

    added unpublished/published modal + filter logic

--- packages/ui/src/pages/AdminPage/components/AdminTaskLibrary.vue
+++ packages/ui/src/pages/AdminPage/components/AdminTaskLibrary.vue
@@ -3,16 +3,43 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/vue-query";
 import taskAPI from "@/api/tasks";
 import { ElImage, ElNotification } from "element-plus";
 import { useBatteryEditingStore } from "../../../stores/admin.ts";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 
 const queryClient = useQueryClient();
 const batteryEditingStore = useBatteryEditingStore();
 
 const filterMenu = ref(false);
+const showUnpublished = ref(true);
+const showPublished = ref(false);
 
 function toggleMenu() {
   filterMenu.value = !filterMenu.value;
 }
+
+function toggleUnpublished() {
+  showUnpublished.value = !showUnpublished.value;
+}
+
+function togglePublished() {
+  showPublished.value = !showPublished.value;
+}
+
+const filteredTasks = computed(() => {
+  if (!data.value) return [];
+
+  if (!showUnpublished.value && !showPublished.value) {
+    return [];
+  }
+
+  if (showUnpublished.value && showPublished.value) {
+    return data.value;
+  }
+
+  return data.value.filter((task) => {
+    if (showPublished.value) return task.published === true;
+    if (showUnpublished.value) return task.published === false;
+  });
+});
 
 const uploadMutation = useMutation(taskAPI.uploadBattery, {
   onSuccess: () => {
@@ -69,20 +96,47 @@ function handleFileUpload(event: Event) {
           Upload File</label
         >
       </div>
-      <div>
+      <div class="relative inline-block">
         <ElImage
           src="/icons/filter.svg"
           class="self-center"
           @click="toggleMenu"
         />
+        <div
+          v-if="filterMenu"
+          class="absolute top-full -translate-x-[-6px] -translate-y-[12px] z-50 flex flex-col gap-2 rounded-lg bg-white shadow-[0_2px_8px_rgba(15,20,31,0.15)] p-3 w-[140px]"
+        >
+          <div
+            class="flex items-center gap-2 cursor-pointer"
+            @click="toggleUnpublished"
+          >
+            <ElImage
+              :src="
+                showUnpublished
+                  ? '/icons/checked-box.svg'
+                  : '/icons/unchecked-box.svg'
+              "
+              class="self-center"
+            />Unpublished
+          </div>
+          <div
+            class="flex items-center gap-2 cursor-pointer"
+            @click="togglePublished"
+          >
+            <ElImage
+              :src="
+                showPublished
+                  ? '/icons/checked-box.svg'
+                  : '/icons/unchecked-box.svg'
+              "
+              class="self-center"
+            />Published
+          </div>
+        </div>
       </div>
-      <!-- <div v-if="filterMenu" class="flex gap-2">
-        <ElImage src="/icons/unchecked-box.svg" class="self-center" @click="" />
-        <ElImage src="/icons/checked-box.svg" class="self-center" @click="" />
-      </div> -->
       <ElScrollbar>
         <div class="flex-1 flex flex-col gap-4">
-          <div v-for="task in data" :key="task._id">
+          <div v-for="task in filteredTasks" :key="task._id">
             <div
               :class="[
                 'flex gap-5 p-4 border rounded-2xl cursor-pointer',
