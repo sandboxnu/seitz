f60644e3b1275af1c300b553e6fee4f51f80fce3
JacobLef <jacoblefk@gmail.com>
2025-10-17 23:01:49 -0400

    Initial attempt at boot and start scripts completed. General structure laid out for cleanup, infra, and program/application start is done.

---
+++ scripts/start.py
@@ -0,0 +1,10 @@
+import subprocess
+
+def start():
+  try:
+    subprocess.run(["pnpm", "--stream", "-r", "dev"], check=True)
+  except subprocess.CalledProcessError:
+    print("Error starting application!")
+    exit(1)
+
+start()
