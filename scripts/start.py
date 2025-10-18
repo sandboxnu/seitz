1275a676b2eecbf07ad8373395221e4c48a8b004
JacobLef <jacoblefk@gmail.com>
2025-10-18 10:50:53 -0400

    Updated boot script to correctly shut down everything without having error messages.

--- scripts/start.py
+++ scripts/start.py
@@ -1,10 +1,10 @@
 import subprocess
 
-def start():
+def init():
   try:
     subprocess.run(["pnpm", "--stream", "-r", "dev"], check=True)
   except subprocess.CalledProcessError:
     print("Error starting application!")
     exit(1)
 
-start()
+init()
