0e3e3c92a097a23b9bc31a9dc098d0517d0e9a6b
jolinhuang224 <huang.jol@northeastern.edu>
2025-10-26 03:33:32 -0400

    back button for studybuilder page

--- packages/ui/src/pages/StudyBuilderPage/StudyBuilderPage.vue
+++ packages/ui/src/pages/StudyBuilderPage/StudyBuilderPage.vue
@@ -19,30 +19,55 @@ if (!authStore.currentUser) {
 
 const studyBuilderStore = useStudyBuilderStore();
 const taskEditingStore = useTaskEditingStore();
+
+function returnToConditions() {
+  const id = studyBuilderStore.studyId;
+  if (id) {
+    router.push({ name: "conditions", params: { id } });
+  } else {
+    // Fallback: simple history back if id is missing for some reason
+    router.back();
+  }
+}
 </script>
 
 <template>
-  <!-- This is sort of a hack to delete elements if they are dropped outside of sessions -->
-  <Draggable
-    v-loading="studyBuilderStore.isStudySaving"
-    class="flex-1 flex overflow-auto"
-    ghost-class="hidden"
-    :group="{ put: ['taskbar', 'session'] }"
-    item-key=""
-  >
-    <template #header>
-      <StudyPanel class="grow basis-[750px] shrink-0" />
-      <TaskBank class="flex-none" />
-    </template>
-    <template #item></template>
-  </Draggable>
-  <AppEditModal
-    :visible="taskEditingStore.editingTaskId !== undefined"
-    header="Edit Task"
-    sub-header="Customize your task"
-    @cancel="taskEditingStore.editingTaskId = undefined"
-    @done="taskEditingStore.editingTaskId = undefined"
-  >
-    <TaskEditPanel />
-  </AppEditModal>
+  <div>
+    <button
+      class="inline-flex items-center text-neutral-600 mx-4 mt-4"
+      type="button"
+      @click="returnToConditions"
+    >
+      <img
+        src="../../../public/icons/back-button.svg"
+        alt="Back"
+        class="h-4 w-4"
+      />
+      <span>Back</span>
+    </button>
+
+    <!-- This is sort of a hack to delete elements if they are dropped outside of sessions -->
+    <Draggable
+      v-loading="studyBuilderStore.isStudySaving"
+      class="flex-1 flex overflow-auto"
+      ghost-class="hidden"
+      :group="{ put: ['taskbar', 'session'] }"
+      item-key=""
+    >
+      <template #header>
+        <StudyPanel class="grow basis-[750px] shrink-0" />
+        <TaskBank class="flex-none" />
+      </template>
+      <template #item></template>
+    </Draggable>
+    <AppEditModal
+      :visible="taskEditingStore.editingTaskId !== undefined"
+      header="Edit Task"
+      sub-header="Customize your task"
+      @cancel="taskEditingStore.editingTaskId = undefined"
+      @done="taskEditingStore.editingTaskId = undefined"
+    >
+      <TaskEditPanel />
+    </AppEditModal>
+  </div>
 </template>
