8ed8c87a27c14926d0eaf581175edb6378fbfab6
jolinhuang224 <huang.jol@northeastern.edu>
2025-11-17 07:07:12 -0500

    session card UI changes & fix task and quantity

--- packages/ui/src/pages/StudyConditions/components/components/SessionCard.vue
+++ packages/ui/src/pages/StudyConditions/components/components/SessionCard.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import { useStudyBuilderStore } from "@/stores/studyBuilder";
 
 const studyBuilderStore = useStudyBuilderStore();
@@ -11,6 +12,32 @@ const props = defineProps<{
   };
   draggable: boolean;
 }>();
+
+// Creates list of unique task types (by task id)
+const uniqueTasksAll = computed(() => {
+  const seen = new Set<string>();
+  const uniqueTasks: { _id: string; task: string; quantity: number }[] = [];
+  for (const t of props.session.tasks) {
+    if (!seen.has(t.task)) {
+      seen.add(t.task);
+      uniqueTasks.push(t);
+    }
+  }
+  return uniqueTasks;
+});
+
+// Shows at most three tasks in session card (unique)
+const tasksToShow = computed(() => uniqueTasksAll.value.slice(0, 3));
+
+// Calculates and maps quantity of each task to its id
+const taskCounts = computed(() => {
+  const counts = new Map<string, number>();
+  for (const t of props.session.tasks) {
+    const id = t.task;
+    counts.set(id, (counts.get(id) || 0) + 1);
+  }
+  return counts;
+});
 </script>
 
 <template>
@@ -30,12 +57,13 @@ const props = defineProps<{
       <div class="text-neutral-500 font-bold text-base">
         {{ props.session.name }}
       </div>
-      <ElImage src="/icons/lock.svg" fit="cover" class="w-4 h-4 ml-auto" />
     </div>
 
-    <div class="inline-flex flex-col justify-start items-start gap-3">
+    <div
+      class="inline-flex flex-col justify-between items-start gap-3 border-l border-neutral-300 pl-3 w-full ml-10"
+    >
       <div
-        v-for="element in props.session.tasks"
+        v-for="element in tasksToShow"
         :key="element._id"
         class="inline-flex justify-start items-center"
       >
@@ -46,11 +74,11 @@ const props = defineProps<{
               : studyBuilderStore.getTaskName(element.task)
           }}
         </div>
-        <div class="text-stone-900 text-xs">...{{ element.quantity }}</div>
+        <div class="text-stone-900 text-xs">
+          ...{{ taskCounts.get(element.task) || 1 }}
+        </div>
       </div>
-    </div>
-    <div v-if="props.session.tasks.length > 3" class="text-black text-xs">
-      ...
+      <div v-if="uniqueTasksAll.length > 3" class="text-black text-xs">...</div>
     </div>
   </div>
 </template>
